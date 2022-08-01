24981767447d77e678b767d53dcd639e02b15b39
becastro <benatcastro@gmail.com>
2022-08-01 02:25:29 +0200

    gui v-0

--- srcs/push_swap/check_args.c
+++ srcs/push_swap/check_args.c
@@ -6,11 +6,36 @@
 /*   By: bena <bena@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 11:27:46 by becastro          #+#    #+#             */
-/*   Updated: 2022/07/19 00:21:13 by bena             ###   ########.fr       */
+/*   Updated: 2022/08/01 02:24:03 by bena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static void	ft_duplicate_arg(char **args, char *c_arg)
+{
+	int	i;
+	int	arg;
+	int	tmp;
+	int	check;
+
+	check = 0;
+	arg = ft_atoi(c_arg);
+	i = -1;
+	while (args[++i])
+	{
+		tmp = ft_atoi(args[i]);
+		if (arg == tmp)
+			check++;
+		else if (arg == tmp && check != 0)
+		{
+			ft_putstr_fd("Error, argument number ", 1);
+			ft_putnbr_fd(i, 1);
+			ft_putstr_fd(" appears more than 1 time ❌\n", 1);
+			exit (EXIT_FAILURE);
+		}
+	}
+}
 
 void	ft_check_args(int count, char **args)
 {
@@ -28,11 +53,12 @@ void	ft_check_args(int count, char **args)
 		j = -1;
 		while (args[i][++j])
 		{
+			ft_duplicate_arg(args, args[i]);
 			if (!ft_isdigit(args[i][j]))
 			{
 				ft_putstr_fd("Error, argument number ", 1);
 				ft_putnbr_fd(i, 1);
-				ft_putstr_fd(" isn't a digit\n", 1);
+				ft_putstr_fd(" isn't a digit ❌\n", 1);
 				exit (EXIT_FAILURE);
 			}
 		}
