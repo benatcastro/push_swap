640b19276b42747f6d89b32ebfb436dc54a7d6f5
becastro <benatcastro@gmail.com>
2022-08-19 19:37:50 +0200

    median fnc done

--- srcs/push_swap/sorters/median.c
+++ srcs/push_swap/sorters/median.c
@@ -10,13 +10,57 @@
 
 #include "push_swap.h"
 
-static
+
+/**
+ * @brief
+ * checks if an array of integers is sorted
+ * @param tab -> array to be checked
+ * @param sz -> size of the array
+ * @return true
+ * @return false
+ */
+static bool	is_sorted(int *tab, int sz)
+{
+	int	i;
+
+	i = -1;
+
+	while (tab[++i] && i < sz && i + 1 < sz)
+		if (tab[i] > tab[i + 1])
+			return (false);
+	return (true);
+}
+
+/**
+ * @brief
+ * Binary sorting of tab
+ * @param tab -> array of ints to be sorted
+ */
+static void	sort_tab(int *tab, int sz)
+{
+	int	i;
+	int	tmp;
+
+	i = -1;
+	while (tab[++i] && i < sz)
+	{
+		if (tab[i] > tab[i + 1] && i + 1 < sz)
+		{
+			tmp = tab[i];
+			tab[i] = tab[i + 1];
+			tab[i + 1] = tmp;
+		}
+	}
+	if (!is_sorted(tab, sz))
+		sort_tab(tab, sz);
+}
 
 int	ft_get_median(t_stack **stack)
 {
 	t_stack	*aux;
 	int		i;
 	int		*tab;
+	int		median;
 
 	tab = ft_calloc(ft_lst_size(stack), sizeof(int));
 	aux = (*stack);
@@ -28,6 +72,8 @@ int	ft_get_median(t_stack **stack)
 		i++;
 		aux = aux->next;
 	}
-
-	return (0);
+	sort_tab(tab, ft_lst_size(stack));
+	median = tab[i % 2];
+	free(tab);
+	return (median);
 }
