d1b7c87635dcadf223b60c0b2b6361de8048a8da
benatcastro <benatcastro@gmail.com>
2022-08-29 20:32:30 +0200

    clean

--- srcs/push_swap/sorters/defined_cases.c
+++ srcs/push_swap/sorters/defined_cases.c
@@ -6,7 +6,7 @@
 /*   By: becastro <becastro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 03:44:43 by becastro          #+#    #+#             */
-/*   Updated: 2022/08/29 20:29:10 by becastro         ###   ########.fr       */
+/*   Updated: 2022/08/29 20:31:00 by becastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,12 +72,9 @@ void	ft_sort_5_logic(t_data *data)
 {
 	if (ft_check_short(&data->stack_a))
 		return ;
-	//ft_show_double_list(&data->stack_a, &data->stack_b);
 	ft_moveto_head(&data->stack_a, NULL, ft_find_min_nbr(&data->stack_a));
-	//printf("Pushing (%d) (%d)\n",data->stack_a->key, data->stack_a->n);
 	ft_pb(&data->stack_a, &data->stack_b);
 	ft_moveto_head(&data->stack_a, NULL, ft_find_min_nbr(&data->stack_a));
-	//printf("Pushing (%d) (%d)\n", data->stack_a->key, data->stack_a->n);
 	ft_pb(&data->stack_a, &data->stack_b);
 	ft_sort_3_logic(&data->stack_a);
 	ft_pa(&data->stack_a, &data->stack_b);
