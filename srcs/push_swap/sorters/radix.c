1e2dcbd6c867180f328d5616191f0fbdbdcd943a
benatcastro <benatcastro@gmail.com>
2022-09-13 13:59:03 +0200

    works, amy winehouse

--- srcs/push_swap/sorters/radix.c
+++ srcs/push_swap/sorters/radix.c
@@ -6,25 +6,54 @@
 /*   By: becastro <becastro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 06:32:31 by becastro          #+#    #+#             */
-/*   Updated: 2022/09/09 08:34:07 by becastro         ###   ########.fr       */
+/*   Updated: 2022/09/13 13:49:56y becastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_back_to_a(__attribute__((unused))t_stack **head_a, t_stack **head_b, t_data *data)
+static int	ft_max_key(t_stack *stack)
+{
+	int		biggest;
+
+	biggest = stack->key;
+	while (stack)
+	{
+		if (biggest < stack->key)
+			biggest = stack->key;
+		stack = stack->next;
+	}
+	return (biggest);
+}
+
+int	ft_max_key_binary(t_stack *head)
+{
+	int	iteration;
+	int	biggest;
+
+	biggest = ft_max_key(head);
+	iteration = 0;
+	while (biggest)
+	{
+		biggest >>= 1;
+		iteration++;
+	}
+	return (iteration);
+}
+
+void	ft_amy_winehouse(t_data *data)
 {
 	t_stack	*aux;
 	int		i;
 	int		iterations;
 
-	iterations = ft_lst_size(head_b);
+	iterations = ft_lst_size(&data->stack_b);
 	i = 0;
-	aux = *head_b;
+	aux = data->stack_b;
 	while (i < iterations)
 	{
 		ft_pa(data);
-		aux = *head_b;
+		aux = data->stack_b;
 		i++;
 	}
 }
@@ -54,13 +83,12 @@ void	ft_radix(t_data *data)
 	size_t	bit_pos;
 
 	bit_pos = 0;
-	i = data->sz_a;
 	ft_short_keys(data);
+	i = ft_max_key_binary(data->stack_a);
 	while (i--)
 	{
 		ft_bitmove(data, bit_pos);
-		ft_back_to_a(&data->stack_a, &data->stack_b, data);
+		ft_amy_winehouse(data);
 		bit_pos++;
 	}
-	// ft_show_double_list(&data->stack_a, &data->stack_b);
 }
